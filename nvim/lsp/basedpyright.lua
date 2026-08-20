d646c98db9b6b1ab073e27d673276a27ebaf57d7
Dr00pyd00 <kottakcode@gmail.com>
2026-08-20 12:43:27 +0400

    feat( templates)  pyrightconf for django

---
+++ nvim/lsp/basedpyright.lua
@@ -0,0 +1,15 @@
+return {
+  cmd = { "basedpyright-langserver", "--stdio" },
+  filetypes = { "python" },
+  root_markers = { "pyproject.toml", "setup.py", "manage.py", ".git" },
+  settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        typeCheckingMode = "standard",
+      },
+    },
+  },
+}
+
